4efabc127e51a6655eb62ceddff059ae2324bad7
IFADUOLA SAMUEL <85873317+Ifaduolas@users.noreply.github.com>
2023-05-08 14:16:16 +0100

    Thanks to Sunil BK

---
+++ src/components/data/Data.js
@@ -0,0 +1,319 @@
+
+export const nav = [
+    {
+      text: "home",
+      path: "/",
+    },
+    {
+      text: "about",
+      path: "/about",
+    },
+    {
+      text: "services",
+      path: "/services",
+    },
+    {
+      text: "blog",
+      path: "/blog",
+    },
+    {
+      text: "pricing",
+      path: "/pricing",
+    },
+    {
+      text: "contact",
+      path: "/contact",
+    },
+  ]
+  export const featured = [
+    {
+      cover: "../images/hero/h1.png",
+      name: "Family House",
+      total: "122 Property",
+    },
+    {
+      cover: "../images/hero/h2.png",
+      name: "House & Villa",
+      total: "155 Property",
+    },
+    {
+      cover: "../images/hero/h3.png",
+      name: "Apartment",
+      total: "300 Property",
+    },
+    {
+      cover: "../images/hero/h4.png",
+      name: "Office & Studio",
+      total: "80 Property",
+    },
+    {
+      cover: "../images/hero/h6.png",
+      name: "Villa & Condo",
+      total: "80 Property",
+    },
+  ]
+  export const list = [
+    {
+      id: 1,
+      cover: "../images/list/p-1.png",
+      name: "Parkview Estate, Ikoyi, Lagos",
+      location: "26 Olusegun Aina St, Ikoyi",
+      category: "For Rent",
+      price: "₦100,000",
+      type: "Apartment",
+    },
+    {
+      id: 2,
+      cover: "../images/list/p-2.png",
+      name: "Living Gold Estate",
+      location: "Agodogba St, Ikoyi Lagos",
+      category: "For Sale",
+      price: "₦400,000,000",
+      type: "Condos",
+    },
+    {
+      id: 3,
+      cover: "../images/list/p-7.png",
+      name: "The Real Estate Corner",
+      location: "5, Adewale Kolawole Cress",
+      category: "For Rent",
+      price: "₦70,000",
+      type: "Offices",
+    },
+    {
+      id: 4,
+      cover: "../images/list/p-4.png",
+      name: "Herringbone Realty",
+      location: "Abisogun Rd",
+      category: "For Sale",
+      price: "₦320,000,000",
+      type: "Homes & Villas",
+    },
+    {
+      id: 5,
+      cover: "../images/list/p-5.png",
+      name: "Brick Lane Realty",
+      location: "21 Ramart Cress Ogudu GRA Lagos",
+      category: "For Rent",
+      price: "₦2,000,000",
+      type: "Commercial",
+    },
+    {
+      id: 6,
+      cover: "../images/list/p-6.png",
+      name: "Banyon Tree Realty",
+      location: "Ikeja GRA Lagos ",
+      category: "For Sale",
+      price: "₦500,000000",
+      type: "Apartment",
+    },
+  ]
+  export const awards = [
+    {
+      icon: <i class='fa-solid fa-trophy'></i>,
+      num: "32 M	",
+      name: "Blue Burmin Award",
+    },
+    {
+      icon: <i class='fa-solid fa-briefcase'></i>,
+      num: "43 M",
+      name: "Mimo X11 Award",
+    },
+    {
+      icon: <i class='fa-solid fa-lightbulb'></i>,
+      num: "51 M",
+      name: "Australian UGC Award",
+    },
+    {
+      icon: <i class='fa-solid fa-heart'></i>,
+      num: "42 M",
+      name: "IITCA Green Award",
+    },
+  ]
+  export const location = [
+    {
+      id: 1,
+      name: "Eko Atlantic",
+      Villas: "12 Villas",
+      Apartments: "10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-1.jpeg",
+    },
+    {
+      id: 2,
+      name: "Banana Island",
+      Villas: "12 Villas",
+      Apartments: "10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-2.jpeg",
+    },
+    {
+      id: 3,
+      name: "KingsWay Road Ikoyi",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-3.jpeg",
+    },
+    {
+      id: 4,
+      name: "Banana Island",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-4.jpeg",
+    },
+    {
+      id: 5,
+      name: "Banana Island",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-5.jpg",
+    },
+    {
+      id: 6,
+      name: "ParkView Estate",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-6.jpg",
+    },
+  ]
+  export const team = [
+    {
+      list: "50",
+      cover: "../images/customer/team-1.jpeg",
+      address: "Island, Mainland",
+      name: "Sargam S. Singh",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "70",
+      cover: "../images/customer/team-2.jpg",
+      address: "Ajah, Lekki",
+      name: "Harijeet M. Siller",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "80",
+      cover: "../images/customer/team-3.jpg",
+      address: "Lekki, Ikoyi",
+      name: "Anna K. Young",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "51",
+      cover: "../images/customer/team-4.jpeg",
+      address: "2272 Briarwood Drive",
+      name: "Michael P. Grimaldo",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "42",
+      cover: "../images/customer/team-5.jpg",
+      address: "2272 Briarwood  GRA Ikeja",
+      name: "Michael P. Grimaldo",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "38",
+      cover: "../images/customer/team-6.jpeg",
+      address: "Montreal, Ogudu GRA",
+      name: "Adam K. Jollio",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+  ]
+  export const price = [
+    {
+      plan: "Basic",
+      price: " 250,000",
+      ptext: "per user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "99.5% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "120GB CDN Bandwidth",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "5GB Cloud Storage",
+        },
+        { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+        { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Enterprise SLA" },
+      ],
+    },
+    {
+      best: "Best Value",
+      plan: "Standard",
+      price: " 500,000",
+      ptext: "per user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "99.5% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "150GB CDN Bandwidth",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "10GB Cloud Storage",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Personal Help Support",
+        },
+        {
+          change: "color",
+          icon: <i class='fa-solid fa-x'></i>,
+          text: "Enterprise SLA",
+        },
+      ],
+    },
+    {
+      plan: "Platinum",
+      price: " 1,000,000",
+      ptext: "2 user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "100% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "200GB CDN Bandwidth",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "20GB Cloud Storage",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Personal Help Support",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Enterprise SLA",
+        },
+      ],
+    },
+  ]
+  export const footer = [
+    {
+      title: "LAYOUTS",
+      text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+    },
+    {
+      title: "ALL SECTIONS",
+      text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+    },
+    {
+      title: "COMPANY",
+      text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+    },
+  ]
